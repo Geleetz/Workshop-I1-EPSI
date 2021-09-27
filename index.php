3312a5175576ee6e6fb36797a39b64025473db22
cguitteny <91465040+cguitteny@users.noreply.github.com>
2021-09-27 14:04:20 +0200

    Add index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+  <html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Efficient</title>
+
+  </head>
+  <body>
+    <h1>Hello wolrd (or not)</h1>
+  </body>
+</html>
