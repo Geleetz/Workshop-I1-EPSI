6b6cc41be8d055fa0fa8565b215f4c1628df14d0
fbruckmann <florian.bruckmann66@gmail.com>
2021-09-28 10:17:55 +0100

    Add text rules

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
   </head>
   <body>
     <h1>Accueil</h1>
+    <h1>Règles</h1>
   </body>
   <!-- Ajout des librairies JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
