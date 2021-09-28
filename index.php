0cdd305b76f757e7c12e72a68816dbe12ef6cf17
lorosco <leonard.orosco2.0@gmail.com>
2021-09-28 10:22:36 +0200

    first modification on home page

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <!doctype html>
-  <html lang="fr">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Ajout des librairies CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <title>Workshop - Accueil</title>
+<html lang="fr">
 
-  </head>
-  <body>
-    <h1>Accueil</h1>
-  </body>
-  <!-- Ajout des librairies JS -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Ajout des librairies CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  <title>Workshop - Accueil</title>
+
+</head>
+
+<body>
+  <h1>Accueil</h1>
+  <p>Vous allez incarner un employé dans une entreprise sur une journée/semaine durant laquelle vous allez être confonté à
+    différentes situations. Celles-ci mettent en scène des aspects de la cybersécurité auxquels vous pouvez être confronté au
+    cours de votre vie en entreprise. Votre but, répondre au mission qui vous sont demandée tout en respectant les règles pour
+    la sécurité du numérique.</p>
+  <form>
+    <!--type="submit" formaction="https://fr.w3docs.com/"-->
+    <button >Cliquez sur moi</button>
+  </form>
+</body>
+<!-- Ajout des librairies JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
 </html>
