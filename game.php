6e684906039c7bc69f8acdabba4b0ba82e816e69
fbruckmann <florian.bruckmann66@gmail.com>
2021-09-28 10:03:55 +0100

    Modif text index

--- game.php
+++ game.php
@@ -10,7 +10,7 @@
 
   </head>
   <body>
-    <h1>Jeu</h1>
+    <h1>Règles</h1>
   </body>
   <!-- Ajout des librairies JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
