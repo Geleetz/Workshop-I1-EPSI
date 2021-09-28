3f355395dcd9ae5cccfa7ec60faf2604026d9419
fbruckmann <florian.bruckmann66@gmail.com>
2021-09-28 10:45:04 +0100

    modif text index

--- index.php
+++ index.php
@@ -17,14 +17,9 @@
     différentes situations. Celles-ci mettent en scène des aspects de la cybersécurité auxquels vous pouvez être confronté au
     cours de votre vie en entreprise. Votre but, répondre aux missions qui vous sont demandées tout en respectant les règles pour
     la sécurité du numérique.</p>
-  <form>
-    <!--type="submit" formaction="https://fr.w3docs.com/"-->
-<<<<<<< HEAD
-      <button type="button" class="btn btn-danger">Commencez la partie</button>
-=======
-    <button >Cliquez ici pour commencer!</button>
->>>>>>> c837251040d7524ca60d2f9be8b372fd21ddde87
-  </form>
+  <div class="d-grid gap-2 col-6 mx-auto">
+    <button type="button" class="btn btn-outline-danger">Cliquez ici pour commencer!</button>
+  </div>
 </body>
 <!-- Ajout des librairies JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
