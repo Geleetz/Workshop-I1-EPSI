db1949a7a4224ebbf1d50b8d435778bec584e3d5
Guitteny <clement.guitteny@teleflex.com>
2021-09-28 11:46:24 +0200

    start game button

--- index.php
+++ index.php
@@ -18,14 +18,9 @@
   cours de votre vie en entreprise. Votre but, répondre aux missions qui vous sont demandées tout en respectant les règles pour
   la sécurité du numérique.</p>
   <div class="d-grid gap-2 col-6 mx-auto">
-    <button onclick="startGame()" type="button" class="btn btn-outline-danger">Lancer la partie</button>
+    <button onclick="window.location.href='game.php';" type="button" class="btn btn-outline-danger">Lancer la partie</button>
   </div>
 </body>
-<script>
-function startGame() {
-  window.location.href="game.php";
-}
-</script>
 <!-- Ajout des librairies JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </html>
