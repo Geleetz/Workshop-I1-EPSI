729443606f1ee0d9389246f5ad3c1821b09e9974
fbruckmann <florian.bruckmann66@gmail.com>
2021-09-28 12:15:44 +0100

    mise en page

--- index.php
+++ index.php
@@ -14,22 +14,26 @@
 
 </head>
 <body>
-<h2 class="text-center">La cybersécurité au quotidien</h2>
+<h2 class="text-center" style="margin-top: 2%;">La cybersécurité au quotidien</h2>
+<span class="border border-dark">
 <div class="container">
-  <div class="row">
-    <div class="col-md-3"></div>
-    <div class="col-md-6">
-      <p class="text-center">Vous allez incarner un employé dans une entreprise sur une journée ou une semaine durant laquelle vous allez être confronté à
-        différentes situations. Celles-ci mettent en scène des aspects de la cybersécurité auxquels vous pouvez être confronté au
-        cours de votre vie en entreprise. Votre but, répondre aux missions qui vous sont demandées tout en respectant les règles pour
-        la sécurité du numérique.</p>
-      <div class="d-grid gap-2 col-6 mx-auto">
-        <button onclick='window.location.href="game.php"' type="button" class="btn btn-outline-danger">Lancer la partie</button>
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6" style="margin-left: auto; margin-right: auto;">
+        <p class="text-center">Vous allez incarner un employé dans une entreprise sur une journée ou une semaine durant laquelle vous allez être confronté à
+          différentes situations. Celles-ci mettent en scène des aspects de la cybersécurité auxquels vous pouvez être confronté au
+          cours de votre vie en entreprise. Votre but, répondre aux missions qui vous sont demandées tout en respectant les règles pour
+          la sécurité du numérique.</p>
+        <div class="d-grid gap-2 col-6 mx-auto">
+          <button style="margin: 2%;" onclick='window.location.href="game.php"' type="button" class="btn btn-outline-danger">Lancer la partie</button>
+          <button style="margin: 2%;" onclick='window.location.href="game.php"' type="button" class="btn btn-outline-danger">Lire les règles</button>
+          <button style="margin: 2%;" onclick='window.close' type="button" class="btn btn-outline-danger">Quitter</button>
+        </div>
       </div>
+      <div class="col-md-3"></div>
     </div>
-    <div class="col-md-3"></div>
   </div>
-</div>
+  </span>
 </body>
 <!-- Ajout des librairies JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
