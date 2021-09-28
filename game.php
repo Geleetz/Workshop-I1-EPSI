b3a179e676fae04e81dba0775c4ec5668914d7a6
Guitteny <clement.guitteny@teleflex.com>
2021-09-28 09:57:17 +0200

    Ajout de bootstrap

--- game.php
+++ game.php
@@ -4,11 +4,14 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Efficient</title>
+    <!-- Ajout des librairies CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>Workshop - Jeu</title>
 
   </head>
   <body>
-    <h1>Hello wolrd (or not) 3</h1>
+    <h1>Jeu</h1>
   </body>
+  <!-- Ajout des librairies JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </html>
