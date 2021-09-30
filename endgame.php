5e3af620125885bd1df2fc66f6d134546998814f
Clement Guitteny <clement.guitteny@gmail.com>
2021-09-30 17:07:20 +0200

    changement de l'affichage 4

--- endgame.php
+++ endgame.php
@@ -18,58 +18,6 @@
         <div class="container p-3 my-3 bg-white border border rounded w-75">
           <?= 
           $score = $_POST["score"];
-          if ($score <= 0) {
-            ?>
-            <p style="margin-top: 2%; font-family: pixelfont;">
-              Vous avez peu d'expériences en sécurité, mais vous ne pouvez que vous améliorer !
-            </p>
-            <br>
-            <p style="margin-top: 2%; font-family: pixelfont;">Votre score est : <?= $score; ?></p>
-            <br>
-            <p style="margin-top: 2%; font-family: pixelfont;">Selon vos réponses, on peut voir que vous possédez des lacunes et il faudra faire attention à vous pour ne pas vous faire pièger. Voici quelques liens qui vous serons utiles pour poursuivre des formations ou avoir des conseilles ou règles concernant la cybersécurité :
-            <br><br>
-            https://pix.fr
-            <br>
-            https://www.cnil.fr/professionnel
-            <br>
-            https://www.ssi.gouv.fr
-            </p>
-            <?=
-          } elseif ($score == 1) {
-            ?>
-            <p style="margin-top: 2%; font-family: pixelfont;">
-              Vous avez encore quelques lacunes mais vous êtes sur la bonne voie !
-            </p>
-            <br>
-            <p style="margin-top: 2%; font-family: pixelfont;">Votre score est : <?= $score; ?></p>
-            <br>
-            <p style="margin-top: 2%; font-family: pixelfont;">A partir de vos réponses, on peut en déduire que vous avez encore quelques lacunes. Vous êtes sur la bonne voie alors poursuivez vos efforts ! Voici quelques liens qui vous serons utiles pour poursuivre des formations ou avoir des conseilles ou règles concernant la cybersécurité :
-            <br><br>
-            https://pix.fr
-            <br>
-            https://www.cnil.fr/professionnel
-            <br>
-            https://www.ssi.gouv.fr
-            </p>
-            <?=
-          } elseif ($score => 2) {
-            ?>
-            <p style="margin-top: 2%; font-family: pixelfont;">
-              Félicitations ! Vous êtes un expert en cybersécurité !
-            </p>
-            <br>
-            <p style="margin-top: 2%; font-family: pixelfont;">Votre score est : <?= $score; ?></p>
-            <br>
-            <p style="margin-top: 2%; font-family: pixelfont;">On voit très vite que vous entrenez très bien votre poste de travail, continuez comme ça ! Cependant, si vous voulez approndir vos connaissances, vous pouvez consulter les liens suivants :
-            <br><br>
-            https://pix.fr
-            <br>
-            https://www.cnil.fr/professionnel
-            <br>
-            https://www.ssi.gouv.fr
-            </p>
-            <?=
-          }
           ?>
           <div class="d-grid gap-2 mx-auto" style="margin: 2%;">
             <button style="margin: 2%;" onclick='window.location.href="index.php"' type="button" class="btn btn-outline-primary">Retourner à l'écran d'accueil</button>
