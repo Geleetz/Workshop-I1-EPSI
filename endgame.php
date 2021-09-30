fb05f0a0f097a0fba70080d1d7746af65ad1b9c0
Clement Guitteny <clement.guitteny@gmail.com>
2021-09-30 17:11:09 +0200

    changement de l'affichage 6

--- endgame.php
+++ endgame.php
@@ -17,8 +17,22 @@
         <h2 class="text-center my-5" style="font-family: pixelfont; color: white; text-shadow: 5px 5px #000;">La cybersécurité au quotidien</h2>
         <div class="container p-3 my-3 bg-white border border rounded w-75">
           <?= 
-          $score = $_POST["score"];
+            $score = $_POST["score"];
           ?>
+            <p style="margin-top: 2%; font-family: pixelfont;">
+              Vous avez peu d'expériences en sécurité, mais vous ne pouvez que vous améliorer !
+            </p>
+            <br>
+            <p style="margin-top: 2%; font-family: pixelfont;">Votre score est : <?= $score; ?></p>
+            <br>
+            <p style="margin-top: 2%; font-family: pixelfont;">Selon vos réponses, on peut voir que vous possédez des lacunes et il faudra faire attention à vous pour ne pas vous faire pièger. Voici quelques liens qui vous serons utiles pour poursuivre des formations ou avoir des conseilles ou règles concernant la cybersécurité :
+            <br><br>
+            https://pix.fr
+            <br>
+            https://www.cnil.fr/professionnel
+            <br>
+            https://www.ssi.gouv.fr
+            </p>
           <div class="d-grid gap-2 mx-auto" style="margin: 2%;">
             <button style="margin: 2%;" onclick='window.location.href="index.php"' type="button" class="btn btn-outline-primary">Retourner à l'écran d'accueil</button>
           </div>
