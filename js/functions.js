a7a687d4c220b6d9b271090491f99df80bc8606c
Clement Guitteny <clement.guitteny@gmail.com>
2021-09-30 17:22:31 +0200

    Correction json 2

--- js/functions.js
+++ js/functions.js
@@ -78,8 +78,8 @@ function gameReset() {
 
 function btnProvideQuestion() {
   if(isGameFinished()){
-    var request = new XMLHttpRequest();
-    request.open('POST', 'https://scuisond.fr/endgame.php');
+    //var request = new XMLHttpRequest();
+    //request.open('POST', 'https://scuisond.fr/endgame.php');
     //request.setRequestHeader("Content-Type", "application/json");
     var json = [];
     json['questions'] = JSON.stringify(questions);
@@ -88,7 +88,7 @@ function btnProvideQuestion() {
     json['score'] = JSON.stringify(currentScore);
     console.log(JSON.stringify({'questions': questions, 'answers': totalanswers, 'answer': totalanswer, 'score': currentScore}));
     gameReset();
-    request.send(JSON.stringify({'questions': questions, 'answers': totalanswers, 'answer': totalanswer, 'score': currentScore}));
+    $.post('https://scuisond.fr/endgame.php',JSON.stringify({'questions': questions, 'answers': totalanswers, 'answer': totalanswer, 'score': currentScore}));
   }
 
   var randomNumber = Math.floor(Math.random() * quiz.length);
