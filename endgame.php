8bd16d7b56cb4871ac5ab97fc86ecdcae04d8356
fbruckmann <florian.bruckmann66@gmail.com>
2021-10-01 09:55:01 +0100

    Feat: delete score

--- endgame.php
+++ endgame.php
@@ -19,17 +19,17 @@
           <?php 
           if ($_POST["score"] <= 0) {
             echo "<p style='margin-top: 2%; font-family: pixelfont;'>Vous avez peu d'expériences en sécurité, mais vous ne pouvez que vous améliorer !</p>
-            <br><p style='margin-top: 2%; font-family: pixelfont;'>Votre score est :". $_POST["score"] ."</p><br>
+            <br><p style='margin-top: 2%; font-family: pixelfont;'>Votre score est : ". $_POST["score"] ."</p><br>
             <p style='margin-top: 2%; font-family: pixelfont;'>Selon vos réponses, on peut voir que vous possédez des lacunes et il faudra faire attention à vous pour ne pas vous faire pièger. Voici quelques liens qui vous serons utiles pour poursuivre des formations ou avoir des conseilles ou règles concernant la cybersécurité :
             <br><br>https://pix.fr<br>https://www.cnil.fr/professionnel<br>https://www.ssi.gouv.fr</p>";
           } elseif ($_POST["score"] == 1) {
             echo "<p style='margin-top: 2%; font-family: pixelfont;'>Vous avez encore quelques lacunes mais vous êtes sur la bonne voie !</p>
-            <br><p style='margin-top: 2%; font-family: pixelfont;'>Votre score est :". $_POST["score"] ."</p><br>
+            <br><p style='margin-top: 2%; font-family: pixelfont;'>Votre score est : ". $_POST["score"] ."</p><br>
             <p style='margin-top: 2%; font-family: pixelfont;'>A partir de vos réponses, on peut en déduire que vous avez encore quelques lacunes. Vous êtes sur la bonne voie alors poursuivez vos efforts ! Voici quelques liens qui vous serons utiles pour poursuivre des formations ou avoir des conseilles ou règles concernant la cybersécurité :
             <br><br>https://pix.fr<br>https://www.cnil.fr/professionnel<br>https://www.ssi.gouv.fr</p>";
           } elseif ($_POST["score"] >= 2) {
             echo "<p style='margin-top: 2%; font-family: pixelfont;'>Félicitations ! Vous êtes un expert en cybersécurité !</p>
-            <br><p style='margin-top: 2%; font-family: pixelfont;'>Votre score est :". $_POST["score"] ."</p><br>
+            <br><p style='margin-top: 2%; font-family: pixelfont;'>Votre score est : ". $_POST["score"] ."</p><br>
             <p style='margin-top: 2%; font-family: pixelfont;'>On voit très vite que vous entrenez très bien votre poste de travail, continuez comme ça ! Cependant, si vous voulez approndir vos connaissances, vous pouvez consulter les liens suivants :
             <br><br>https://pix.fr<br>https://www.cnil.fr/professionnel<br>https://www.ssi.gouv.fr</p>";
           }
